07b2e734120e094c5dbcc3abbf67af0a06b74b61
set <set>
2024-10-04 00:05:06 +0200

    Færdig

--- Programmering/Lektion_7/script.py
+++ Programmering/Lektion_7/script.py
@@ -1,3 +1,4 @@
+#Test af om grid64x48 virker, hvilket den gør, husk at uploade til repository efter ændringer, ellers virker det ikke. 
 from main import create_grid_64x48
 
 grid = create_grid_64x48()

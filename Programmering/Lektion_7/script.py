2ac0ef9b6e64df78a2963c56e0ee0303d3d7876f
set <set>
2024-10-04 14:49:26 +0200

    Tester

--- Programmering/Lektion_7/script.py
+++ Programmering/Lektion_7/script.py
@@ -1,12 +1,14 @@
 #Test af om grid64x48 virker, hvilket den gør, husk at uploade til repository efter ændringer, ellers virker det ikke. 
 from main import create_grid_64x48
-
-grid = create_grid_64x48()
-
-
 from main import draw_horizontal
 
-draw_horizontal(1,1,10)
+
+#Lav et grid
+grid = create_grid_64x48()
+
+#Tegn en vandret linje
+
+grid = draw_horizontal(grid, y=1,x_start = 1,x_end = 10)
 
 
 

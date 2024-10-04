cffff4b4e2368d861540f5c68bf8c06eb936466b
set <set>
2024-10-05 01:08:00 +0200

    Ændringer og færdige maze

--- Programmering/Lektion_7/main.py
+++ Programmering/Lektion_7/main.py
@@ -7,11 +7,12 @@ def start_pygame(screen_size):
     return screen
 
 def handle_event_exit():
-    """Handler for events som at slukke spillet"""
+    """Handler for events som at lukke spillet"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return False
-        return True
+            return False  # Returnér False for at stoppe spillet
+    return True  # Fortsæt spillet ellers
+
 
 def update_screen(screen, color):
     """Opdatere skærmen"""
@@ -28,7 +29,7 @@ def create_grid_64x48():
 
 def draw_horizontal(grid, row, start_col, end_col):
     """Vandret, ændrer fx række 10, fra kolonne 5 til 15"""
-    for col in range(start_col, end_col + 1):
+    for col in range(start_col, end_col + 1): #Plus 1 skyldes at det vil give det rigtige antal, så man ikke skal tænke på at sætte ekstra ind
         grid[row][col] = 1  # Sætter 1-taller fra kolonne 5 til 15 i række 10
 
 
@@ -36,5 +37,5 @@ def draw_horizontal(grid, row, start_col, end_col):
 
 def draw_vertical(grid, col, start_row, end_row):
     """Lodret, ændrer fx kolonne 20 fra række 5 til 15"""
-    for row in range(start_row, end_row + 1):
+    for row in range(start_row, end_row + 1): #Plus 1 skyldes at det vil give det rigtige antal, så man ikke skal tænke på at sætte ekstra ind
         grid[row][col] = 1  # Sætter 1-taller fra række 5 til 15 i kolonne 20
---
+++ Programmering/Lektion_7/maze_færdige_produkt.py
@@ -0,0 +1,47 @@
+import pygame
+import random
+from main import create_grid_64x48
+from main import draw_horizontal
+from main import draw_vertical
+from main import handle_event_exit
+
+block_size = 10
+grid = create_grid_64x48()
+
+
+#Ramme til maze
+draw_horizontal(grid, row=0, start_col=0, end_col=63)
+draw_horizontal(grid, row=47, start_col=0, end_col=63)
+draw_vertical(grid, col=63, start_row=0, end_row=47)
+
+#Laver alle verticale linjer i mazen
+for i in range(0, 62, 2):
+    draw_vertical(grid, col=i, start_row=0, end_row=47)
+
+
+#Laver en random maze, ved at ændre 1 værdier til 0 random hver anden gang, i takt med at alle blev lavet om til 1
+#grid[row][col]
+for i in range(2,61,2):
+    random_numbers = random.randint(1,46)
+    grid[random_numbers][i]=0
+
+#Starter pygame
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+
+running = True
+
+while running:
+    screen.fill((255,255,255))
+    #Gennemgår hver celle i row og col og hvis værdierne er 1, så fylder den området med sort
+    for row in range(48):
+        for col in range(64):
+            if grid[row][col] == 1:
+                pygame.draw.rect(screen, (0, 0, 0), (col * block_size, row * block_size, block_size, block_size))
+            
+
+    pygame.display.flip()
+
+    running = handle_event_exit
+
+pygame.quit()

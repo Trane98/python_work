4357833d7888667ad05f653283d54a55371fba17
set <set>
2024-10-04 23:52:40 +0200

    ombytning af funktioner

--- Programmering/Lektion_7/maze.py
+++ Programmering/Lektion_7/maze.py
@@ -4,12 +4,17 @@ from main import draw_horizontal
 from main import draw_vertical
 
 block_size = 10
-grid = create_grid_64x48
+grid = create_grid_64x48()
 
 
-draw_vertical(grid, row=20, start_col=5, end_col=15)
-draw_horizontal(grid, col=10, start_row=5, end_row=15)
+#Ramme til maze
+draw_vertical(grid, row=0, start_col=0, end_col=63)
+draw_vertical(grid, row=47, start_col=0, end_col=63)
+draw_horizontal(grid, col=0, start_row=0, end_row=47)
+draw_horizontal(grid, col=63, start_row=0, end_row=47)
 
+for i in range(0, 11, 2):
+    draw_vertical(grid, row=i, start_col=0, end_col=63)
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))

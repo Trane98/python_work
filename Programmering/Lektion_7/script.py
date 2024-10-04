5518a2822d4687e2a1b1dcbaf6ccd3a1802d99b5
set <set>
2024-10-04 20:19:26 +0200

    jhgfb

--- Programmering/Lektion_7/script.py
+++ Programmering/Lektion_7/script.py
@@ -3,13 +3,12 @@ from main import create_grid_64x48
 from main import draw_vertical
 
 
-#Lav et grid
+# Opret grid
 grid = create_grid_64x48()
 
-#Tegn en vandret linje
+# Tegn en lodret linje fra y=1 til y=10 på kolonne x=5
+grid = draw_vertical(grid, x=5, y_start=1, y_end=10)
 
-grid = draw_vertical(grid, x=1,y_start = 1,y_end = 10)
-
-
-
-print(grid)
+# Print grid for at se ændringerne
+for row in grid:
+    print(row)

dc36d9be19587ee21f6848810034e3b452f47f88
set <set>
2024-10-30 18:38:33 +0100

    hgjfgh

--- Miniprojekt2/Breadth_First_search_Test.py
+++ Miniprojekt2/Breadth_First_search_Test.py
@@ -1,6 +1,4 @@
 import pygame
-import sys
-from queue import Queue
 from main import Terrain_Types
 from main import create_grid_64x48
 from main import add_neighbors
@@ -9,28 +7,16 @@ from main import find_path
 #Laver grid 64x48
 grid_64x48 = create_grid_64x48()
 cell_size = 10
-start = (1, 1)
-goal = (62, 46)
-key_state = 0
 
-#Danner grundlag for terrain types og deres tilhørende værdier (Farve og grid-nummer)
+
+#Klasserne og deres tilhørende værdier (Farve og tilhørende grid-nummer)
 grass = Terrain_Types((30, 200, 30), 0)
 wall = Terrain_Types((0, 0, 0), 1)
 
-#Definerer en funktion til at finde gyldige naboer
-def add_neighbors(current):
-    neighbors = []
-    x, y = current
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  #Op, højre, ned, venstre
-
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < 64 and 0 <= ny < 48:  #Sørger for at være indenfor grænserne
-            if grid_64x48[nx][ny] == grass.grid_number:  #Tjekker om cellen er græs altså = 0
-                neighbors.append((nx, ny))
-    return neighbors
 
 
+#Starter med at lave gras (Fejlsikring som defualt setting, da det ellers vil lukke ned, hvis man trykker på skærmen)
+key_state = grass.grid_number
 
 #Starts pygame
 pygame.init()
@@ -41,7 +27,7 @@ running = True
 path = []
 
 while running:
-    screen.fill((255, 255, 255))  #This is not important
+    screen.fill((255, 255, 255))  #Kan egentlig bare slettes til sidst
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -50,16 +36,16 @@ while running:
         #Registrer 0,1 og space, til at ændre i grid eller starte algorithmen
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_0:
-                key_state = 0
+                key_state = grass.grid_number
                 print("Key 0 has been pressed, and its special ability has been activated")
             elif event.key == pygame.K_1:
-                key_state = 1
+                key_state = wall.grid_number
                 print("Key 1 has been pressed, and its special ability has been activated")
             elif event.key == pygame.K_SPACE:
                 path = find_path((1,1),(62,46))  #Find path when spacebar is pressed
                 print("Pathfinding algorithm activated")
 
-    #Detects left mouse click to change terrain
+    #Registrer om venstre musseklik presses
     if pygame.mouse.get_pressed()[0]:
         pos = pygame.mouse.get_pos()
         col = pos[0] // cell_size

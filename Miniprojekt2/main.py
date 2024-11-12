2c0edfbd6454fe91e8bf776620d905a0100f0b61
set <set>
2024-11-12 18:09:25 +0100

    hgfd

--- Miniprojekt2/main.py
+++ Miniprojekt2/main.py
@@ -85,6 +85,36 @@ def find_path(start,goal):
 
     return path
 
+def dijkstra(graph, start, goal):
+    """Funktionen her implementerer BFS for at finde den koreste sti fra start til mål"""
+    from queue import PriorityQueue
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+    
+    for next in graph.neighbors(current):
+        new_cost = cost_so_far[current] + graph.cost(current, next)
+        if next not in cost_so_far or new_cost < cost_so_far[next]:
+            cost_so_far[next] = new_cost
+            priority = new_cost
+            frontier.put(next, priority)
+            came_from[next] = current
+
+    return came_from
+
+
+
+
+
 
 def draw_horizontal(grid_64x48, row, start_col, end_col):
     """Vandret, ændrer fx række 10, fra kolonne 5 til 15"""
